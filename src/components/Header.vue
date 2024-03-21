4418f3712c27fd3a28ab65ac5f6617e6de510a80
s4r44d43 <sararez@live.it>
2024-03-21 15:55:44 +0100

    [starting] header style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,9 +3,45 @@
 </script>
 
 <template>
+
+<header class="center">
+    <section class="conteiner header">
+      
+      <div class="options">
+        <span>Donna</span>
+        <span>Uomo</span>
+        <span>Bambino</span>
+      </div>
+
+      <div class="logo cont">
+        <img class="logo" src="./img/boolean-logo.png" alt="logo">
+      </div>
+
+      <div>
+        logino
+      </div>
+
+    </section>
+  </header>
   
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+
+  @use '../assets/scss/partials/_utilities';
+
+  header{
+    height: 80px;
+    background-color: rgba(255,105,0,255);
+      .conteiner.header{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        max-height: 100%;
+        .options{
+          padding-right: 10px;
+        }
+      }
+  }
 
 </style>
